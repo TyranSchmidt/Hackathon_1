8c7381d0adaf7e3d917b8d2b37dce49e470e995b
TyranSchmidt <tyransch@gmail.com>
2020-04-18 18:13:23 +0300

    done

---
+++ hackathon1p2.js
@@ -0,0 +1,13 @@
+let form = document.getElementsByTagName("form");
+
+let input = document.getElementsByClassName("input1");
+
+let btn_submit = document.getElementById("btn_input");
+btn_submit.addEventListener("click", newRow);
+
+function newRow() {
+	for(values of input) {
+		if(values != "")
+			console.log(hello);
+	}
+}
